eda8e5000960c552a546f3937b5f5a16b29641a9
Ayush Soni <soni.ayush.2212@gmail.com>
2025-05-03 14:05:59 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Working");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`);
+});
